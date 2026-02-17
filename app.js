441a676c0fbc4badcb07026881e5aa3cb029dc11
medo962006 <ahmedadel01100847101@gmail.com>
2026-02-17 22:45:45 +0200

    Add Dockerfile

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const unused = 'this will cause a lint error';
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
